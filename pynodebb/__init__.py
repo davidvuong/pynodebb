8c9310765d58c138fc13f94cc9144c6107709912
David Vuong <david.vuong256@gmail.com>
2015-09-20 11:56:57 +1000

    chore(pynodebb): set build as v0.0.1dev

--- pynodebb/__init__.py
+++ pynodebb/__init__.py
@@ -10,4 +10,4 @@ from pynodebb.client import Client
 
 
 __version_info__ = (0, 0, 1,)
-__version__ = '.'.join(map(str, __version_info__))
+__version__ = '.'.join(map(str, __version_info__)) + 'dev'
