b81cace1ff346f305d936e825d22218b68736c51
David Vuong <david.vuong256@gmail.com>
2015-10-12 14:08:40 +1100

    fix(setup.py): pip install pynodebb fails
    It fails when requests isn't already installed.

--- pynodebb/__init__.py
+++ pynodebb/__init__.py
@@ -6,7 +6,17 @@ Copyright (c) 2015 David Vuong <david.vuong256@gmail.com>
 Licensed MIT
 """
 from __future__ import unicode_literals
-from pynodebb.client import Client
+
+# NOTE: Avoid initial import errors.
+#
+# `setup.py` imports tvm (__version__) before installing dependencies. As a result,
+# there are dependencies imported in `pynodebb.client` which may not exist yet.
+#
+# e.g. `import requests`.
+try:
+    from pynodebb.client import Client
+except ImportError:
+    Client = None
 
 
 __version_info__ = (0, 0, 2,)
